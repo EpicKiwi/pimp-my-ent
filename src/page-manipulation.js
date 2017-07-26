fcf283d6d864c7074dec2e5f5070a5ee84682a37
Baptiste <EpicKiwi@users.noreply.github.com>
2017-07-26 13:31:42 +0200

    Update page-manipulation.js

--- src/page-manipulation.js
+++ src/page-manipulation.js
@@ -4,7 +4,8 @@
   $(".layout__slot:nth-child(2)").attr("id","app-list");
   $(".layout__slot:nth-child(3)").attr("id","activity-list");
   $(".layout__slot:nth-child(4)").attr("id","tweets-list");
-  $(".layout__slot:nth-child(4)").attr("id","tweets-list");
+  $(".layout__slot:nth-child(5)").attr("id","campus-post-list");
+  $(".layout__slot:nth-child(6)").attr("id","school-post-list");
   
   var appList = $("#app-list")
   
