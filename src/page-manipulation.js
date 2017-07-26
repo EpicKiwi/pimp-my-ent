61f5a792f62257f75ef03612cffbce430d6d3aa1
Baptiste <EpicKiwi@users.noreply.github.com>
2017-07-26 11:03:35 +0200

    Create page-manipulation.js

---
+++ src/page-manipulation.js
@@ -0,0 +1,9 @@
+(function($){
+
+  $(".layout__slot:nth-child(1)").attr("id","calendar-list");
+  $(".layout__slot:nth-child(2)").attr("id","app-list");
+  $(".layout__slot:nth-child(3)").attr("id","activity-list");
+  $(".layout__slot:nth-child(4)").attr("id","tweets-list");
+  $(".layout__slot:nth-child(4)").attr("id","tweets-list");
+  
+})(jQuery)
